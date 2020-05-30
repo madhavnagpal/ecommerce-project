120a6cc51d88d6a246bc46a01b3d5ac4b2bd727d
Madhav Nagpal <mr.madhavnagpal@gmail.com>
2020-05-30 15:07:34 +0530

    basic cart added
    Signed-off-by: Madhav Nagpal <mr.madhavnagpal@gmail.com>

--- client/src/components/login/Cart.js
+++ client/src/components/login/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { LoginContext } from "../../contexts/LoginContext";
 import { CartContext } from "../../contexts/CartContext";
 import { useHistory } from "react-router-dom";
@@ -7,13 +7,26 @@ import CartItem from "./CartItem";
 export default function Cart() {
   const [loggedInStatus, setLoginStatus] = useContext(LoginContext);
   const [cart, setCart] = useContext(CartContext);
+  const [totalPrice, setTotalPrice] = useState(0);
   const history = useHistory();
+
+  useEffect(() => {
+    let dummyCart = [...cart];
+    let newTotalPrice = dummyCart.reduce(
+      (acc, cartItem) => acc + cartItem.price * cartItem.quantity,
+      0
+    );
+    setTotalPrice(newTotalPrice);
+  }, [cart]);
 
   if (loggedInStatus.isLoggedIn) {
     return (
       <div className="container my-3 border border-primary  rounded">
-        <h3 className="text-capitalize">Welcome {loggedInStatus.name} !</h3>
-        <div className="">
+        <h4 className="text-center text-capitalize">
+          Welcome {loggedInStatus.name} !
+        </h4>
+        <h4 className="text-center">Total Price : &#8377;{totalPrice}</h4>
+        <div className="row">
           {cart.map((cartItem) => (
             <CartItem key={cartItem.id} cartItem={cartItem} />
           ))}
--- client/src/components/login/CartItem.js
+++ client/src/components/login/CartItem.js
@@ -1,39 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
 
 export default function CartItem({ cartItem }) {
+  const [cart, setCart] = useContext(CartContext);
+
+  const handleRemoveFromCart = (e) => {
+    const newCart = cart.filter(
+      (oldCartItem) => cartItem.id !== oldCartItem.id
+    );
+    setCart(newCart);
+  };
   return (
-    <div className="row">
-      <div className="col-md-4 col-sm-6 text-center p-0  border rounded my-3">
-        <div style={{ paddingBottom: "120px" }}>
-          <img src={cartItem.avatar} width="150" height="220" alt="cartItem " />
-          <p
-            className="text-wrap  text-capitalize  mt-3"
-            style={{ fontSize: "1.2rem" }}
-          >
-            {cartItem.name}
-            <br />
-            Price : &#8377;{cartItem.price}
-            <br />
-          </p>
-          <div>
-            Quantity
-            {/* <input
-              type="number"
-              min="1"
-              style={quantityStyle}
-              onChange={handleQuantityChange}
-              value={quantity}
-            /> */}
-          </div>
-          {/* <button
-            className="btn btn-danger"
-            onClick={handleAddToCart}
-            style={cartBtnStyle}
-          > */}
-          {/* Add To Cart
-          </button> */}
-        </div>
+    <div className="col-md-4 col-sm-6 text-center p-0  border rounded my-3">
+      <div style={{ paddingBottom: "40px" }}>
+        <img src={cartItem.avatar} width="150" height="220" alt="cartItem " />
+        <p
+          className="text-wrap  text-capitalize  mt-3"
+          style={{ fontSize: "1.2rem" }}
+        >
+          {cartItem.name}
+          <br />
+          Price : &#8377;{cartItem.price}
+          <br />
+        </p>
       </div>
+      <div>
+        Quantity : {cartItem.quantity}
+        <br />
+        Total : {cartItem.quantity * cartItem.price}
+      </div>
+      <button
+        className="btn btn-block btn-danger"
+        onClick={handleRemoveFromCart}
+      >
+        Remove from Cart
+      </button>
     </div>
   );
 }
